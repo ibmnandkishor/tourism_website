fe74573e2f4d1808dbc891f334d410607851b8a1
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-07-26 09:02:24 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); 
 $_SESSION = array();
-if (ini_get("session.use_cookies")) {
+if (ini_get("session.use_cookies")) { 
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 60*60,
         $params["path"], $params["domain"],
